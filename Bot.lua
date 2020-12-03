a94776926a8aca3f0e1b03d868f5c0c925ee11fa
ScriptItWithCode <62032154+ScriptItWithCode@users.noreply.github.com>
2020-12-03 16:27:56 +0100

    k

--- Bot.lua
+++ Bot.lua
@@ -1,19 +1,37 @@
 local Token = require("GetEnv")("Token")
-local ManToken = require("GetEnv")("Man+Token")
+local ManToken = require("GetEnv")("ManToken")
 
 local Discordia = require("discordia")
 local Client = Discordia.Client()
 local ManClient = Discordia.Client()
 
+local API = require("API")
+
 _G.Client = Client
 _G.ManClient = ManClient
 
-Client:run("Bot " .. Token)
-Client:waitFor("ready")
+coroutine.wrap(function()
 
-ManClient:run("Bot " .. ManToken)
-ManClient:waitFor("ready")
+    Client:run("Bot " .. Token)
+    Client:waitFor("ready")
+    print("Client ready!")
 
-local ToDM = Client:getUser("533536581055938580")
+    ManClient:run("Bot " .. ManToken)
+    ManClient:waitFor("ready")
+    print("ManClient ready!")
 
+    for i, v in pairs(ManClient.guilds) do 
+        v:delete()
+    end
 
+    print("Deleted guilds")
+
+    local ToDM = Client:getUser("533536581055938580")
+
+    local NewServer = API:New("test")
+
+    ToDM:send("http://discord.gg/" .. NewServer:CreateInvite().code)
+
+    print("sent")
+
+end)()
--- libs/API.lua
+++ libs/API.lua
@@ -6,9 +6,67 @@ function Module:New(JobId)
 
     local ManClient = _G.ManClient
 
-    local Guild = ManClient:createGuild()
-	
-	NewServer.Invite = 
+    ManClient:createGuild("CubeVoice Server: " .. JobId)
+    local Guild
+
+    ManClient:once("guildCreate", function(GuildGotten)
+
+        Guild = GuildGotten
+
+    end)
+
+    ManClient:waitFor("guildCreate")
+
+    for i, v in pairs(Guild.categories) do
+        v:delete()
+    end
+
+    for i, v in pairs(Guild.voiceChannels) do
+        v:delete()
+    end
+
+    for i, v in pairs(Guild.textChannels) do
+        v:delete()
+    end
+
+    local EveryoneRole = Guild.roles:toArray()[1]
+    EveryoneRole:disablePermissions(0x00004000, 0x00008000, 0x00000001, 0x04000000, 0x00040000, 0x00020000)
+    --print(EveryoneRole)
+
+    ManClient:on("voiceChannelLeave", function(Member)
+        
+        if not Member.voiceChannel then
+            Member.user:send("You disconnected!")
+            Member:kick("You disconnected!")
+        end
+
+    end)
+
+    local Role = Guild:createRole("HOST")
+    Role:hoist()
+
+    Guild.me:addRole(Role.id)
+
+    local TextCat = Guild:createCategory("Text")
+
+    local NoMicChat = TextCat:createTextChannel("nomic")
+    local BotChat = TextCat:createTextChannel("bot")
+
+    local VoiceCat = Guild:createCategory("Voice")
+
+    local JoinChat = VoiceCat:createVoiceChannel("join")
+    
+    local RoomsCat = Guild:createCategory("Rooms")
+
+    NewServer.JoinChat = JoinChat
+    NewServer.NoMicChat = NoMicChat
+    NewServer.BotChat = BotChat
+
+    function NewServer:CreateInvite()
+        return JoinChat:createInvite({temporary = false, max_uses = 1})
+    end
+    
+    return NewServer
 
 end
 
